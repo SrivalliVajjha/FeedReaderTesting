19d7fb543f659690a263b825cddbcc63e5662dd1
SrivalliVajjha <valli.sri1996@gmail.com>
2017-02-15 12:04:33 +0530

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,19 +54,33 @@ $(function() {
          beforeEach(function(done){
            loadFeed(0,done);  
          });
-         it('the Load feed complete work',function(){
+         it(' the Load feed complete work',function(){
              expect($('.entry')[0]).toBeDefined();
          });
      });
 
     describe('New Feed Selection',function(){
+        var feed1,feed2;
         beforeEach(function(done){
-            loadFeed(1,done);
+            loadFeed(0,function(){
+                feed1=$('.feed').html();
+                done();
+           
+            loadFeed(1,function(){
+                feed2=$('.feed').html();
+                done();
+            });
+                 });
         });
         it('has the new newsfeed loaded',function(){
-            expect($('.header-title').html()).toBe('CSS Tricks');
+            expect(feed1).not.toBe(feed2);
         });
     });
 
-    
+    /* TODO: Write a new test suite named "New Feed Selection"
+
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
 }());
